271be9f0d7e3df9b152de2a528fbbca93d635598
Hunter Chang <hunter@madwiremedia.com>
2019-09-29 10:30:24 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <button
+      type="button"
+      className="bg-blue-100 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Button
+    </button>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
